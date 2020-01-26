1a12b6e6b4066ed8d44f156d7ca88f04a6226cb2
SolaJr <35401733+SolaJr@users.noreply.github.com>
2020-01-26 09:16:53 -0600

    Add files via upload

---
+++ lib/ui/earnings_details.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class EarningsDetailsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return EarningsDetailsView();
+  }
+}
+
+class EarningsDetailsView extends StatefulWidget {
+  @override
+  _EarningsDetailsViewState createState() => _EarningsDetailsViewState();
+  // See the world around yout! :)
+}
+
+class _EarningsDetailsViewState extends State<EarningsDetailsView> {
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.black,
+          title: Text("Weekly"),
+        ),
+        body: ListView(
+          children: <Widget>[
+          ],
+        ));
+  }
+}
